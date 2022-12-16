8f6f8b38d82e82754287cacb51e509d2af736dd2
Aarav238 <aarav8090shukla@gmail.com>
2022-12-16 15:22:19 +0530

    components updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState, useEffect } from "react";
+
+import { StreamChat } from "stream-chat";
+import { Chat, Channel,Window,ChannelHeader,MessageInput,MessageList  } from "stream-chat-react";
+import 'stream-chat-react/dist/css/index.css'
+
+const API_KEY = process.env.REACT_APP_API_KEY
+
+const USER1 = {
+  id: "user1",
+  name: "user1",
+  image: "https://i.pravatar.cc/300"
+};
+const USER2 = {
+  id: "user2",
+  name: "user2",
+  image: "https://i.pravatar.cc/300"
+};
+const USER3 = {
+  id: "user3",
+  name: "user3",
+  image: "https://i.pravatar.cc/300"
+};
+
+const users =  [ USER1, USER2 , USER3];
+
+const getRandomUser = () => {
+  const randomIndex = Math.floor(Math.random() * users.length);
+  return users[randomIndex];
+};
 
 function App() {
+
+  const[chatClient, setChatClient] = useState(null);
+  const[channel,setChannel] = useState(null);
+
+  useEffect(() => {
+   async function initChat(){
+      const client = StreamChat.getInstance(API_KEY);
+
+      const user = getRandomUser();
+
+      client.connectUser(user,client.devToken(user.id));
+      const channel = client.channel("team","general",{
+        name: "General",
+        image:"https://i.pravatar.cc/300"
+      })
+
+      await channel.create()
+
+      channel.addMembers([user.id])
+      setChannel(channel)
+
+      setChatClient(client)
+
+    }
+
+    initChat()
+
+    return () => {
+      if(chatClient) chatClient.disconnectUser();
+    }
+  },[])
+
+  if(!chatClient || !channel) return <></>
   return (
     <div>
-      
+      <Chat client={chatClient} theme={'messaging light'}>
+        <Channel channel={channel}>
+          <Window>
+            <ChannelHeader/>
+            <MessageList/>
+            <MessageInput/>
+          </Window>
+        </Channel>
+      </Chat>
     </div>
   );
 }
